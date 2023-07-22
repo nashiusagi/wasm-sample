226c090143e1e8aec372b6e31a86e277f7f84624
nashiusagi <ritotech0226@gmail.com>
2023-07-22 21:47:17 +0900

    [update] JS内のスネークケースをキャメルケースに変更

--- www/image-processing.js
+++ www/image-processing.js
@@ -1,27 +1,27 @@
-export function averageMosaic(img, width, height, kernel_size, imageData) {
-  const number_h = Math.floor(height / kernel_size)
-  const number_w = Math.floor(width / kernel_size)
-  const num_kernel_pixels = kernel_size * kernel_size
+export function averageMosaic(img, width, height, kernelSize, imageData) {
+  const numberH = Math.floor(height / kernelSize)
+  const numberW = Math.floor(width / kernelSize)
+  const numKernelPixels = kernelSize * kernelSize
 
-  for (let h = 0; h < number_h; h++) {
-    for (let w = 0; w < number_w; w++) {
+  for (let h = 0; h < numberH; h++) {
+    for (let w = 0; w < numberW; w++) {
       let sumR = 0
       let sumG = 0
       let sumB = 0
-      for (let y = kernel_size * h; y < kernel_size * (h + 1); y++) {
-        for (let x = kernel_size * w; x < kernel_size * (w + 1); x++) {
+      for (let y = kernelSize * h; y < kernelSize * (h + 1); y++) {
+        for (let x = kernelSize * w; x < kernelSize * (w + 1); x++) {
           sumR += img[4 * (y * width + x) + 0]
           sumG += img[4 * (y * width + x) + 1]
           sumB += img[4 * (y * width + x) + 2]
         }
       }
 
-      let avgR = Math.floor(sumR / num_kernel_pixels)
-      let avgG = Math.floor(sumG / num_kernel_pixels)
-      let avgB = Math.floor(sumB / num_kernel_pixels)
+      let avgR = Math.floor(sumR / numKernelPixels)
+      let avgG = Math.floor(sumG / numKernelPixels)
+      let avgB = Math.floor(sumB / numKernelPixels)
 
-      for (let y = kernel_size * h; y < kernel_size * (h + 1); y++) {
-        for (let x = kernel_size * w; x < kernel_size * (w + 1); x++) {
+      for (let y = kernelSize * h; y < kernelSize * (h + 1); y++) {
+        for (let x = kernelSize * w; x < kernelSize * (w + 1); x++) {
           imageData.data[4 * (y * width + x) + 0] = avgR
           imageData.data[4 * (y * width + x) + 1] = avgG
           imageData.data[4 * (y * width + x) + 2] = avgB
--- www/index.js
+++ www/index.js
@@ -9,31 +9,31 @@ imageInput.addEventListener('change', (e) => {
   const image = new Image()
   image.onload = async () => {
     const { width, height } = image
-    const orig_canvas = document.getElementById('original_canvas')
-    orig_canvas.width = width
-    orig_canvas.height = height
+    const origCanvas = document.getElementById('original_canvas')
+    origCanvas.width = width
+    origCanvas.height = height
 
-    const wasm_canvas = document.getElementById('canvas_wasm')
-    wasm_canvas.width = width
-    wasm_canvas.height = height
+    const wasmCanvas = document.getElementById('canvas_wasm')
+    wasmCanvas.width = width
+    wasmCanvas.height = height
 
-    const js_canvas = document.getElementById('canvas_js')
-    js_canvas.width = width
-    js_canvas.height = height
+    const jsCanvas = document.getElementById('canvas_js')
+    jsCanvas.width = width
+    jsCanvas.height = height
 
-    orig_canvas.getContext('2d').drawImage(image, 0, 0, width, height)
-    const { data } = orig_canvas.getContext('2d').getImageData(0, 0, width, height)
+    origCanvas.getContext('2d').drawImage(image, 0, 0, width, height)
+    const { data } = origCanvas.getContext('2d').getImageData(0, 0, width, height)
 
     console.log('js only')
-    let js_context = js_canvas.getContext('2d')
-    let js_imageData = js_context.createImageData(width, height)
+    let jsContext = jsCanvas.getContext('2d')
+    let jsImageData = jsContext.createImageData(width, height)
 
     const jsGenerateStartTime = Date.now()
-    averageMosaic(data, width, height, 12, js_imageData)
+    averageMosaic(data, width, height, 12, jsImageData)
     const jsGenerateEndTime = Date.now()
 
     const jsDrawStartTime = Date.now()
-    await js_context.putImageData(js_imageData, 0, 0)
+    await jsContext.putImageData(jsImageData, 0, 0)
     const jsDrawEndTime = Date.now()
 
     console.log(`\tgenerate:js\telapsed:${jsGenerateEndTime - jsGenerateStartTime}[ms]`)
